a45657b6343bbf6851cc305fa58a0df8656ea89d
Katharine C Humble <katharinechumble@gmail.com>
2021-09-04 13:44:55 -0400

    added user menu

--- server.js
+++ server.js
@@ -20,9 +20,57 @@ const promptMessage = {
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3001,
+    //login credentials, using dotenv to secure password
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 },
 console.log('Connected to the database')
 );
+
+connection.connect(err => {
+    if (err) throw err;
+    userMenu();
+});
+
+// use inquirer to give users menu options
+
+function userMenu() {
+    inquirer.prompt({
+        name: 'menu',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            promptMessages.viewAllEmployees,
+            promptMessages.viewByDepartment,
+            promptMessages.viewByManager,
+            promptMessages.viewAllRoles,
+            promptMessages.addEmployee,
+            promptMessages.removeEmployee,
+            promptMessages.updateRole,
+            promptMessages.exit
+        ]
+    }).then(answer => {
+        console.log('answer', answer);
+        switch (answer.action){
+            case promptMessages.viewAllEmployees:
+                viewAllEmployees();
+                break;
+            case promptMessages.viewByDepartment:
+                viewByDepartment();
+                break;
+            case promptMessages.viewByManager:
+                viewByManager();
+                break;
+            case promptMessages.addEmployee:
+                addEmployee();
+                break;
+            case promptMessages.removeEmployee:
+                remove('delete');
+                break;
+            case promptMessages.updateRole:
+                updateRole();
+                break;
+        }
+    })
+}
