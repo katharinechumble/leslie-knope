899198d4e2e8040a3c4be0cc3d831fde937c196c
Katharine C Humble <katharinechumble@gmail.com>
2021-09-04 14:39:41 -0400

    added functions to view and sort employees

--- server.js
+++ server.js
@@ -62,6 +62,9 @@ function userMenu() {
             case promptMessages.viewByManager:
                 viewByManager();
                 break;
+            case promptMessages.viewAllRoles:
+                viewAllRoles();
+                break;
             case promptMessages.addEmployee:
                 addEmployee();
                 break;
@@ -72,5 +75,79 @@ function userMenu() {
                 updateRole();
                 break;
         }
-    })
+    });
+}
+
+// functions to view, filter, and sort employees
+
+function viewAllEmployees() {
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager ON (manager.id = employee.manager_id)
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\n');
+        console.log('VIEW ALL EMPLOYEES');
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
+}
+
+function viewByDepartment(){
+    const query = `SELECT department.name AS department, role.title, employee.id, employee.first_name, employee.last_name
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role_id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY department.name;`;
+    connection.query(query, (err, res) => {
+        if(err) throw err;
+        console.log('\n');
+        console.log('VIEW EMPLOYEE BY DEPARTMENT');
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
+}
+
+function viewByManager() {
+    const query = `SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager, department.name AS department, employee.id, employee.first_name, employee.last_name, role.title
+    FROM employee
+    LEFT JOIN employee manager ON (manager.id = employee.manager_id)
+    INNER JOIN role ON (role.id = employee.role_id && employee.manager_id !='NULL')
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY manager;`;
+    connection.query(query, (err, res) => {
+        if(err) throw err;
+        console.log('\n');
+        console.log('VIEW EMPLOYEE BY MANAGER');
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
+}
+
+function viewAllRoles() {
+    const query = `SELECT role.title, employee.id, employee.first_name, employee.last_name, department.name AS department
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role_id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY role.title;`;
+    connection.query(query, (err, res) => {
+        if(err) throw err;
+        console.log('\n');
+        console.log('VIEW EMPLOYEE BY ROLE');
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
+}
+
+// add functions to add, remove, and edit employees
+
+async function addEmployee() {
+
 }
